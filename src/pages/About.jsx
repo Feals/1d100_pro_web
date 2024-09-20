436e7a0e99d001347a8fea1abb8b204153b0d64c
Feals <ax.juste@gmail.com>
2024-09-20 18:01:10 +0200

    add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,70 @@
 import Layout from "./Layout";
-
+import "../assets/css/about.css";
 function About() {
   return (
     <Layout>
-      <h2>A PROPOS</h2>
+      <section id="about">
+        <div id="title_page">
+          <h1>A PROPOS</h1>
+          <div id="rectangle1"></div>
+          <div id="rectangle2"></div>
+          <div id="rectangle3"></div>
+          <div id="rectangle4"></div>
+        </div>
+        <article>
+          <img src="/images/dicesbox.png" alt="Boite de dé" />
+          <p>
+            Am evelles quia pres debis reped quatem hariatat aditaecabori rat
+            reri voluptios magnis dolorro bla qui re volupti core veliqua ssunti
+            qui- dige necesequo omnimen tiorepe rciisti nulluptureri ulpa quia
+            voluptatius pore nusdae pos am, apis aut quibus alit ut prenihilis
+            modi blabo. Voluptaquis il id que non experia tisimus ape rat
+            facepel lupturia conserum rent omnihillabo. Iquiatur, conem et et,
+            et moluptat faccaep erruptatet, tem voluptatent acest, aliberum
+            quiberro moluptiasit estorectur aspella nihitas dolum quideleceria
+            ipsanim inctas incitiandi rendem quas dolor sitisti berat. Musdam
+            rescium nia sitatem eossequia del ipsume min consece ratet, nobist
+            ut duciliqui consedio. Et aciduntiosam vollese quiasime aut audam ut
+            id quae nonsequi odignatibus molenimi, omni omnim siti vit exeria
+            sed et mos aut modit as doluptate ne landitas ea volorpo remperrum
+            il ellaces tiumquidus nos nes assum eum con rem quatiame volorpor
+            magnia sitiis et im fuga. Nam harumquias inum as et om- nisqui volut
+            posam expliquae es moditem ullacil loreprepe plam a qui omniet,
+            oditaturio to et fugita derspidel et qui blam ut venis dionsed
+            quibusdae. Simusae nist utassi sitate laborep ercidus volupta
+            tibusda prem cum aut quam rehendi omnitatent repelest, volo ipsape
+            dolup- ta tquasperum ipictur? Ut quisciis alit, con et qui
+            remporrorum lat. Si im facitiur, nos quaturias soloris ne
+            velessecabo. Nequia sapicipsunti comnitae vel mosae occum non conet
+            alictio tem ne mi, occate pel iliquost incit, solupta dendi dus
+            ditis dunt voloriatia voluptae conectate remque volut ut placest aut
+            faccaboribus dipiciunto to ommod ut ea doluptae esciatis sitio.
+            Nullor andaere ssuntiscil il inctem ut offic temquas re voluptati
+            unt re ea necuscimint quasitae autem eiuntiuntio et qui aliamentios
+            et, voloribus. Eque con cumque volumquae cus dolorep ernatis erum
+            qui iliquo core ratur secus, corem uta que non re, cus, offictotas
+            sunt labori dipit la sundempore nobit alitaep ratius venissi resseni
+            comnihil imincieni nulla non niendam, est, aspero omnis aditem
+            faceprae pa posanim oluptae ptatem resti berferume quae nonecus
+            nonsequ atinvendiae nobis nis auda non conestrum ius quidionseque
+            sus estiate officid ma soloria doluptis sum rem nem velectia
+            delectiis etus quiasin velendio. Ut quas nulpa quasperibus dolecti
+            de as molendae vel mo magnimos eaquati reped quas rem explit
+            apelliquiam quos sae volor aut laborum incim litatum ad qui cuptaquo
+            et apis aut omnihic tet voluptat ent. Ectem iust volo bero ipsae cum
+            fuga. Ut fugitis doluptas dolorporerum voluptatem is et min rero is
+            et lantia dentiss imillor rorrum ipsum, sitiatecab ipid quamet
+            evellabo. Et odit volorepe deleseditia nimagnia sit, none peditas
+            accatum rernation porum, evero consequis porup- tassit aliciam vit
+            rem. Itatio et volore cusa sam, id quis doluptis dit doluptam
+            quistiu santiusa ium si in ellique voluptur abo. Totas magna- turist
+            exceste niassus. Accati officiis precae as que volor re quam quae
+            eossiminctem explaccum est litias eossim aceate pe lam sint laccabo.
+            Ut qui consenihic torrum ellanim quia dunt, sanda alibern atemossim
+            sit facerunt.
+          </p>
+        </article>
+      </section>
     </Layout>
   );
 }
